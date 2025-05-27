0d0bfaab15b122e4a6d610e92367636ed366cf23
vitormdandrade <vitormachadodeandrade@gmail.com>
2025-05-26 23:00:45 -0300

    Update web build with new repository structure

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0641c44aff464baff586a3c024044d12
 "canvaskit/skwasm.wasm": "1c93738510f202d9ff44d36a4760126b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "213a8955372738ed719fd02e33e9be50",
+"flutter_bootstrap.js": "4f195fb408955759e60aeb717fa1c857",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
